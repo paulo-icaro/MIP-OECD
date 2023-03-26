be48c4b5b608c9b7b9ea254909c3704ee2ffa67e
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2023-03-26 18:51:12 -0300

    Montagem de loop para download dos datasets

--- MIP-OCDE.R
+++ MIP-OCDE.R
@@ -2,6 +2,15 @@
 # === Input-Output Tables - OECD Countries === #
 # ============================================ #
 
+# Link com a estrutura do dataset: https://stats.oecd.org/restsdmx/sdmx.ashx/GetDataStructure/IOTS_2021
+# Link onde os dados são consultados: https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/IOTS_2021
+
+# Obs:
+# Aparentemente, diferente de outras bases, não dá para visualizar essa no navegador devido ao limite
+# do tempo de conexão. Mas é sim possível acessar os dados utilizando a função get_dataset
+
+
+
 # ---------------- #
 # --- Packages --- #
 # ---------------- #
@@ -11,14 +20,28 @@ install.packages('OECD')
 # ----------------- #
 # --- Libraries --- #
 # ----------------- #
-library(OECD)           # OECD API
+library(OECD)                                                 # OECD API
 
 
 # ----------------------- #
 # --- Data Extraction --- #
 # ----------------------- #
-# options(timeout = 120)                          # Aumentar o intervalo máximo de busca na URL do dataset
-databse <- get_dataset("IOTS_2021",
-            filter = list(c("TTL"), c('BRA')),
-            start_time = 1998, end_time = 1998)
+# Na funcao get_dataset o argumento filter e uma lista para indicar as dimensoes do dataset que serao consultadas
+
+options(timeout = 120)                                        # Aumentar o intervalo maximo de busca na URL do dataset
+paises <- c('BRA', 'KOR')
+
+
+database <- vector(mode = 'list', length = length(paises))    # Lista que receberá as databases
+
+
+for (i in 1:length(paises)){
+  data_extraction <- get_dataset(dataset = "IOTS_2021",
+                                 filter = list(c("TTL"), paises[i]),
+                                 start_time = 1995,
+                                 end_time = 2018)
+  database[[i]] <- data_extraction
+  rm(data_extraction)
+}
+
 
