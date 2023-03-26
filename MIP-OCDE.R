6d6f0cf9b15561a73392341a77056ce1ccbd79cd
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2023-03-26 17:36:43 -0300

    Ajustes na formatação do code

--- MIP-OCDE.R
+++ MIP-OCDE.R
@@ -1,17 +1,24 @@
-# ------------------------------------------- #
-# --- Input-Output Tables - OECD Contries --- #
-# ------------------------------------------- #
+# ============================================ #
+# === Input-Output Tables - OECD Countries === #
+# ============================================ #
 
-# Packages
+# ---------------- #
+# --- Packages --- #
+# ---------------- #
 install.packages('OECD')
 
-# Libraries
-library(OECD)     # OECD API
+
+# ----------------- #
+# --- Libraries --- #
+# ----------------- #
+library(OECD)           # OECD API
 
 
-
-# Data Extraction
-get_dataset("IOTS_2021",
+# ----------------------- #
+# --- Data Extraction --- #
+# ----------------------- #
+# options(timeout = 120)                          # Aumentar o intervalo máximo de busca na URL do dataset
+databse <- get_dataset("IOTS_2021",
             filter = list(c("TTL"), c('BRA')),
             start_time = 1998, end_time = 1998)
 
