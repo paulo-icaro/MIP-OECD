78fd0fd00f663a84afe1aff41d9f9525bc3fbfcb
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2023-03-27 14:59:41 -0300

    Preambulo de um loop para análises

--- MIP-OCDE.R
+++ MIP-OCDE.R
@@ -3,11 +3,11 @@
 # ============================================ #
 
 # Link com a estrutura do dataset: https://stats.oecd.org/restsdmx/sdmx.ashx/GetDataStructure/IOTS_2021
-# Link onde os dados são consultados: https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/IOTS_2021
+# Link onde os dados sao consultados: https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/IOTS_2021
 
 # Obs:
-# Aparentemente, diferente de outras bases, não dá para visualizar essa no navegador devido ao limite
-# do tempo de conexão. Mas é sim possível acessar os dados utilizando a função get_dataset
+# Aparentemente, diferente de outras bases, nao da para visualizar essa no navegador devido ao limite
+# do tempo de conexao. Mas e sim possivel acessar os dados utilizando a funcao get_dataset
 
 
 
@@ -22,7 +22,9 @@ install.packages('OECD')
 # ----------------- #
 library(OECD)                                                 # OECD API
 library(openxlsx)
-
+library(readxl)
+path = 'C:/Users/paulo.costa/Downloads/OCDE'
+path = 'D:/Backup - Icaro/Documentos/Repositórios/Projetos/MIP/MIP-OECD'
 
 # ----------------------- #
 # --- Data Extraction --- #
@@ -30,13 +32,14 @@ library(openxlsx)
 # Na funcao get_dataset o argumento filter e uma lista para indicar as dimensoes do dataset que serao consultadas
 
 options(timeout = 180)                                        # Aumentar o intervalo maximo de busca na URL do dataset
-paises <- c('BRA', 'KOR')
+paises <- c('BRA', 'KOR')						                          # Variavel com os nomes dos paises
+dim_col <- read_excel(path= paste0(path, '/Dimensões.xlsx'), sheet = 'coluna', col_names = TRUE)
+dim_row <- read_excel(path= paste0(path, '/Dimensões.xlsx'), sheet = 'linha', col_names = TRUE)
+
+database <- vector(mode = 'list', length = length(paises))    # Lista que recebera as databases
 
 
-database <- vector(mode = 'list', length = length(paises))    # Lista que receberá as databases
-
-
-for (i in 1:length(paises)){
+for (p in 1:length(paises)){
   data_extraction <- get_dataset(dataset = "IOTS_2021",
                                  filter = list(c("TTL"), paises[i]),
                                  start_time = 1995,
@@ -47,14 +50,27 @@ for (i in 1:length(paises)){
 
 
 
-# --- Extracao das dimencoes (Linhas-Colunas) das matrizes --- #
-# col_items <- unique(database[[1]][c(1)])
-# row_items <- unique(database[[1]][c(5)])
+# --------------------- #
+# --- Data Analysis --- #
+# --------------------- #
 
-  # Workbook
-  # wb <- createWorkbook(creator = 'pi')
-  # addWorksheet(wb = wb, sheetName = 'coluna')
-  # addWorksheet(wb = wb, sheetName = 'linha')
-  # writeData(wb = wb, sheet = 'coluna' , x = unique(database[[1]][c(1)]))
-  # writeData(wb = wb, sheet = 'linha' , x = unique(database[[1]][c(5)]))
-  # saveWorkbook(wb = wb, file = 'C:/Users/Paulo/Documents/Repositórios/Projetos/MIP/MIP-OECD/Dimensões.xlsx', overwrite = TRUE)
+var_perc_oecd <- vector(mode = 'list', length = length(paises))
+
+for (p in 1:length(paises)){
+  for (i in 1:24)){
+    var_perc_oecd[[]] <- (database[[p]]$ObsValue[i+1]/database[[p]]$ObsValue[i])-1
+  }}
+
+
+
+# --- Extracao das dimencoes (Linhas-Colunas) das matrizes --- #
+#col_items <- unique(database[[1]][c(1)])
+#row_items <- unique(database[[1]][c(5)])
+
+# Workbook
+#wb <- createWorkbook(creator = 'pi')
+#addWorksheet(wb = wb, sheetName = 'coluna')
+#addWorksheet(wb = wb, sheetName = 'linha')
+#writeData(wb = wb, sheet = 'coluna' , x = unique(database[[1]][c(1)]))
+#writeData(wb = wb, sheet = 'linha' , x = unique(database[[1]][c(5)]))
+#saveWorkbook(wb = wb, file = paste0(path, '/Dimensões.xlsx'), overwrite = TRUE)
