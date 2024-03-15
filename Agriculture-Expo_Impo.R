b83e87bb291e561a7346077e80f840d86c73bfad
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2024-03-14 22:15:28 -0300

    Ranking exportações

--- Agriculture-Expo_Impo.R
+++ Agriculture-Expo_Impo.R
@@ -27,14 +27,17 @@ path = getwd()
 # ------------------- #
 
 # Dicionario ISIC
-dicio_isic = read_excel(path = paste0(path, '/Dataset/Dicionario_ISIC.xlsx'))
+#dicio_isic = read_excel(path = paste0(path, '/Dataset/Dicionario_ISIC.xlsx'))
+dicio_isic = read_excel(path ='C:/Users/paulo.costa.SDE/Downloads/Tese/Dicionario_ISIC.xlsx')
 dicio_isic_agro = dicio_isic %>% filter(NO_ISIC_SECAO == 'Agropecuária')
 
 # Dicionario ISIC x NCM
-dicio_isic_ncm = read_excel(path = paste0(path, '/Dataset/Dicionario_NCM_ISIC.xlsx'))
+#dicio_isic_ncm = read_excel(path = paste0(path, '/Dataset/Dicionario_NCM_ISIC.xlsx'))
+dicio_isic_ncm = read_excel(path = 'C:/Users/paulo.costa.SDE/Downloads/Tese/Dicionario_NCM_ISIC.xlsx')
 dicio_isic_ncm_agro = dicio_isic_ncm %>% filter(CO_ISIC_CLASSE %in% dicio_isic_agro$CO_ISIC_CLASSE)
 
-
+# Dicionario NCM
+dicio_NCM = read_excel(path = 'C:/Users/paulo.costa.SDE/Downloads/Tese/Dicionario_NCM.xlsx')
 
 
 
@@ -43,19 +46,19 @@ dicio_isic_ncm_agro = dicio_isic_ncm %>% filter(CO_ISIC_CLASSE %in% dicio_isic_a
 # --------------------- #
 
 # Exportacoes
-expo = read.csv(file = paste0(path, '/Dataset/Exportacoes_Completas_(1997-2024).csv'), sep = ';')   # Base - Exportacoes
+expo = read.csv(file = 'C:/Users/paulo.costa.SDE/Downloads/Tese/Exportacoes_Completas_(1997-2024).csv', sep = ';')   # Base - Exportacoes
 agro_expo = expo %>%  filter(CO_NCM %in% dicio_isic_ncm_agro$CO_NCM)                                # Base - Exportacoes - Agropecuaria
-total_expo = expo[c(1,11)] %>% group_by(CO_ANO) %>% summarize(Total_Expo = sum(VL_FOB))             # Exporta?oes p/ Ano - Total
+total_expo = expo[c(1,11)] %>% group_by(CO_ANO) %>% summarize(Total_Expo = sum(VL_FOB))             # Exportacoes p/ Ano - Total
 total_agro_expo = agro_expo[c(1,11)] %>% group_by(CO_ANO) %>% summarize(Total_Expo = sum(VL_FOB))   # Exportacoes p/ Ano - Agropecuaria
-perc_expo_agro = total_agro_expo[c(2)]/total_expo[c(2)]                                         # Exportacoes (%) - Agropecuaria
+perc_expo_agro = total_agro_expo[c(2)]/total_expo[c(2)]                                             # Exportacoes (%) - Agropecuaria
 
 
 # Importacoes
-impo = read.csv(file = paste0(path, '/Dataset/Importacoes_Completas_(1997-2024).csv'), sep = ';')   # Base - Importacoes
+impo = read.csv(file = 'C:/Users/paulo.costa.SDE/Downloads/Tese/Importacoes_Completas_(1997-2024).csv', sep = ';')   # Base - Importacoes
 agro_impo = impo %>%  filter(CO_NCM %in% dicio_isic_ncm_agro$CO_NCM)                                # Base - Importacoes - Agropecuaria
 total_impo = impo[c(1,11)] %>% group_by(CO_ANO) %>% summarize(Total_Impo = sum(VL_FOB))             # Importacoes p/ Ano - Total
 total_agro_impo = agro_impo[c(1,11)] %>% group_by(CO_ANO) %>% summarize(Total_Impo = sum(VL_FOB))   # Importacoes p/ Ano - Agropecuaria
-perc_impo_agro = total_agro_impo[c(2)]/total_impo[c(2)]                                         # Importacoes (%) - Agropecuaria
+perc_impo_agro = total_agro_impo[c(2)]/total_impo[c(2)]                                             # Importacoes (%) - Agropecuaria
 
 
 # Tabela resumo
@@ -65,6 +68,12 @@ rownames(expo_impo) = expo_impo[,1]
 expo_impo = expo_impo[,-1]
 
 
+# Produtos mais exportados
+agro_expo_ranking = agro_expo %>% group_by(CO_ANO, CO_NCM) %>% summarize(VL_EXPO = sum(VL_FOB)) %>% slice_max(order_by = VL_EXPO, n = 5)
+agro_expo_ranking = left_join(x = agro_expo_ranking, y = dicio_NCM[c(1,12)], by = 'CO_NCM')
+
+
+# Liberação de memoria
 rm(dicio_isic, dicio_isic_agro, dicio_isic_ncm, expo, agro_expo, total_expo, total_agro_expo, perc_expo_agro, impo, agro_impo, total_impo, total_agro_impo, perc_impo_agro)
 
 
