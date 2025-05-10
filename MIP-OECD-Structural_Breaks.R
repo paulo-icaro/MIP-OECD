4611738fc3827dd0d2f0088714375ffc405d64df
Paulo <pauloicaro38@gmail.com>
2025-05-10 12:26:11 -0300

    Ajustes

--- MIP-OECD-Structural_Breaks.R
+++ MIP-OECD-Structural_Breaks.R
@@ -25,9 +25,17 @@ source(file = paste0(getwd(),'/MIP-OECD-Analysis.R'))     # --- Execucao do scri
 # ------------------------------------- #
 # --- 3. Breaking Test - Bai-Perron --- #
 # ------------------------------------- #
+
+# --- 3.1 - Breaking Test on Techinal Coeficients --- #
+bp_test_results = vector(mode = 'list')
+counter = 0
+
 for (c in length(countries)){                                                               # c para pais
   for (i in 1:45){                                                                          # i para linha
     for (j in 1:45){                                                                        # j para coluna
+      counter = counter + 1
+      
+      # Gathering the Coeficients Series
       for (t in 1:24){                                                                      # t para o ano
         if (t == 1) {w = db[['sectors_coef']][[c]][[t]][i:i,j:j]} 
         else {w <- rbind(w, db[['sectors_coef']][[c]][[t]][i:i,j:j])}
@@ -37,8 +45,9 @@ for (c in length(countries)){                                                   
       ts_w = ts(data = w, frequency = 1, start = 1995)
   
       # Bai-Perron Test
-      bp_test <- breakpoints(ts_w ~ 1)
-      summary(bp_test)
+      bp_test_results[[counter]] <- breakpoints(ts_w ~ 1)
+      #summary(bp_test)
+      names(bp_test_results)[counter] = paste0(dim_row_cod[i,],' to ', dim_col_cod[j,])
     }
   }
 }
